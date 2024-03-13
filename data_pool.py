b1fcc8b326f3b8e4eaf60a5f3329ee84822af1a8
Zaid Alyafeai <alyafey22@gmail.com>
2024-03-13 14:13:51 +0000

    add mawdoo3 and aya to the pool

--- data_pool.py
+++ data_pool.py
@@ -1,6 +1,8 @@
 
 from datasets import load_dataset, concatenate_datasets
+import os 
 
+token = os.getenv('hf_token')
 def process_bacterian(sample):
     if sample['input']:
         sample['instruction'] = sample['instruction']+ ' '+sample['input']
@@ -9,6 +11,18 @@ def process_bacterian(sample):
 
 def process_cidar(sample):
     sample['id'] = f"cidar-v1-{sample['index']}"
+    return sample
+
+def process_mawdoo3(sample):
+    sample['id'] = f"mawdoo3-{sample['index']}"
+    sample['instruction']= sample['question']
+    sample['output'] = sample['answer']
+    return sample
+
+def process_aya(sample):
+    sample['instruction'] = sample['inputs']
+    sample['output'] = sample['targets']
+    sample['id'] = f'aya-{sample["language_code"]}-{sample["index"]}'
     return sample
 
 class Datasets:
@@ -21,11 +35,21 @@ class Datasets:
         cidar = cidar.map(process_cidar)
         self.cidar = self.remove_columns(cidar) 
 
+        mawdoo3 = load_dataset('arbml/mawdoo3', split='train[:5000]', verification_mode='no_checks', token = token)
+        mawdoo3 = mawdoo3.add_column("index", range(len(mawdoo3)))
+        mawdoo3 = mawdoo3.map(process_mawdoo3)
+        self.mawdoo3 = self.remove_columns(mawdoo3)
+
+        aya = load_dataset('arbml/aya_ar', split='train[:5000]', verification_mode = 'no_checks')
+        aya = aya.add_column("index", range(len(aya)))
+        aya = aya.map(process_aya)
+        self.aya = self.remove_columns(aya)
+
     def remove_columns(self, data):
         columns_to_remove = [c for c in data.column_names if c not in ['instruction', 'output', 'id']]
         data = data.remove_columns(columns_to_remove)
         return data
 
     def get_data(self):
-        return  concatenate_datasets([self.cidar, self.bacterian_ar])
+        return  concatenate_datasets([self.cidar, self.bacterian_ar, self.mawdoo3, self.aya])
 
